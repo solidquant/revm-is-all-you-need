ca6e6a4c51c8174fe66427d4bf8296f61c5c637a
Solid Quant <solidquant@gmail.com>
2023-11-20 10:16:12 +0900

    removing anvil

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,8 @@
 use anyhow::{self, Result};
+use ethers::types::U256;
 use fern::colors::{Color, ColoredLevelConfig};
 use log::LevelFilter;
+use rand::Rng;
 
 pub fn setup_logger() -> Result<()> {
     let colors = ColoredLevelConfig {
@@ -27,4 +29,34 @@ pub fn setup_logger() -> Result<()> {
         .apply()?;
 
     Ok(())
+}
+
+pub fn calculate_next_block_base_fee(
+    gas_used: U256,
+    gas_limit: U256,
+    base_fee_per_gas: U256,
+) -> U256 {
+    let gas_used = gas_used;
+
+    let mut target_gas_used = gas_limit / 2;
+    target_gas_used = if target_gas_used == U256::zero() {
+        U256::one()
+    } else {
+        target_gas_used
+    };
+
+    let new_base_fee = {
+        if gas_used > target_gas_used {
+            base_fee_per_gas
+                + ((base_fee_per_gas * (gas_used - target_gas_used)) / target_gas_used)
+                    / U256::from(8u64)
+        } else {
+            base_fee_per_gas
+                - ((base_fee_per_gas * (target_gas_used - gas_used)) / target_gas_used)
+                    / U256::from(8u64)
+        }
+    };
+
+    let seed = rand::thread_rng().gen_range(0..9);
+    new_base_fee + seed
 }
